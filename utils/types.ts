ff15556183cd65acf41f4387fbbab9fd07d473d2
carloshs1 <carlos@ebless.mx>
2022-09-20 13:34:09 -0500

    Create checkout process (shipping and payment routes)

--- utils/types.ts
+++ utils/types.ts
@@ -1,3 +1,7 @@
+import type { NextComponentType } from 'next'
+import { Session } from 'next-auth'
+import { AppProps } from 'next/app'
+
 export type ProductType = {
  slug: string
  image: string
@@ -19,4 +23,18 @@ export type CartItemType = {
 export type LoginFormValues = {
  email: string
  password: string
+}
+
+export type CustomAppProps = AppProps<{
+ session: Session
+}> & {
+ Component: NextComponentType & { auth?: boolean }
+}
+
+export type ShippingFormValues = {
+ fullName: string
+ address: string
+ city: string
+ postalCode: string
+ country: string
 }
