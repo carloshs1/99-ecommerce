9b86992d0fcde5aa7578e4f778133832b21700d4
carloshs1 <carlos@ebless.mx>
2022-09-20 13:43:51 -0500

    Add products to mongodb

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,13 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import User from '../../models/User';
-import data from '../../utils/data';
-import db from '../../utils/db';
+import { NextApiRequest, NextApiResponse } from 'next'
+import Product from '../../models/Product'
+import User from '../../models/User'
+import data from '../../utils/data'
+import db from '../../utils/db'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await db.connect();
-  await User.deleteMany();
-  await User.insertMany(data.users);
-  await db.disconnect();
-  res.send({ message: 'seeded successfully' });
-};
-export default handler;
+ await db.connect()
+ await User.deleteMany()
+ await User.insertMany(data.users)
+ await Product.deleteMany()
+ await Product.insertMany(data.products)
+ await db.disconnect()
+ res.send({ message: 'seeded successfully' })
+}
+export default handler
